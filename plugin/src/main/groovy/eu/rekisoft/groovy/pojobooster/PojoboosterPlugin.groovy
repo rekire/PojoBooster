4fbef1659deb5b7621f1b48e541399129a776560
rekire <git@reki.re>
2016-08-21 23:22:47 +0200

    Make the plugin working. It still does not compile, but that does not matter now.

--- plugin/src/main/groovy/eu/rekisoft/groovy/pojobooster/PojoboosterPlugin.groovy
+++ plugin/src/main/groovy/eu/rekisoft/groovy/pojobooster/PojoboosterPlugin.groovy
@@ -11,10 +11,28 @@ class PojoboosterPlugin implements Plugin<Project> {
         project.configurations.create 'pojobooster'
         project.extensions.create 'pojobooster', PojoboosterPluginExtension
 
+        // TODO this should simplify it and not more complex
+
         project.dependencies.
-                add("compile", project.files("$project.rootDir/annotations/build/libs/annotations.jar"))
+                add("compile", project.getDependencies().create("eu.rekisoft.pojobooster:Annotations:$project.libVersion"))
         project.dependencies.
-                add("pojobooster", project.files("$project.rootDir/preprocessor/build/libs/preprocessor.jar"))
+                add("compile", project.getDependencies().create("eu.rekisoft.pojobooster:Preprocessor:$project.libVersion"))
+        project.dependencies.
+                add("compile", project.getDependencies().create("com.squareup:javapoet:1.7.0"))
+        project.dependencies.
+                add("compile", project.getDependencies().create("com.android.support:support-annotations:24.1.0"))
+        project.dependencies.
+                add("compile", project.getDependencies().create("org.robolectric:android-all:6.0.0_r1-robolectric-0"))
+        project.dependencies.
+                add("pojobooster", project.getDependencies().create("eu.rekisoft.pojobooster:Annotations:$project.libVersion"))
+        project.dependencies.
+                add("pojobooster", project.getDependencies().create("eu.rekisoft.pojobooster:Preprocessor:$project.libVersion"))
+        project.dependencies.
+                add("pojobooster", project.getDependencies().create("com.squareup:javapoet:1.7.0"))
+        project.dependencies.
+                add("pojobooster", project.getDependencies().create("com.android.support:support-annotations:24.1.0"))
+        project.dependencies.
+                add("pojobooster", project.getDependencies().create("org.robolectric:android-all:6.0.0_r1-robolectric-0"))
 
         project.afterEvaluate {
             if (project.plugins.hasPlugin('java')) {
@@ -30,6 +48,7 @@ class PojoboosterPlugin implements Plugin<Project> {
 
     def applyToJavaProject(project) {
         File aptOutputDir = getOutputDir(project)
+        println "do some magic with $aptOutputDir.path"
         project.task('addPojoboosterCompilerArgs') << {
             project.compileJava.options.compilerArgs.addAll '-processorpath',
                     project.configurations.apt.asPath, '-s', aptOutputDir.path
@@ -42,6 +61,8 @@ class PojoboosterPlugin implements Plugin<Project> {
                 logger.info "Generating sources using the annotation processing tool:"
                 logger.info "  Output directory: ${aptOutputDir}"
 
+                variant.javaCompile.classpath += project.configurations.pojobooster
+
                 aptOutputDir.mkdirs()
             }
         }
@@ -53,9 +74,11 @@ class PojoboosterPlugin implements Plugin<Project> {
         def variants
 
         if (project.plugins.hasPlugin('com.android.application')) {
+            println "detected app"
             androidExtension = project.plugins.getPlugin('com.android.application').extension
             variants = androidExtension.applicationVariants
         } else if (project.plugins.hasPlugin('com.android.library')) {
+            println "detected lib"
             androidExtension = project.plugins.getPlugin('com.android.library').extension
             variants = androidExtension.libraryVariants
         } else {
@@ -68,10 +91,13 @@ class PojoboosterPlugin implements Plugin<Project> {
             File aptOutputDir = getOutputDir(project)
             File variantAptOutputDir = project.file("${aptOutputDir}/${dirName}")
 
+            println sourceSetName(variant) + " -- " + androidExtension.sourceSets[sourceSetName(variant)].java.srcDirs
+
+            androidExtension.sourceSets.main.java.srcDirs += "build/generated/source/pojo/" + sourceSetName(variant)
             androidExtension.sourceSets[sourceSetName(variant)].java.srcDirs.addAll variantAptOutputDir.path
 
             javaCompile.options.compilerArgs.addAll '-processorpath',
-                    project.configurations.apt.asPath, '-s', variantAptOutputDir.path
+                    project.configurations.pojobooster.asPath, '-s', variantAptOutputDir.path
 
             javaCompile.source = javaCompile.source.filter {
                 !variant.variantData.extraGeneratedSourceFolders.each { folder ->
