8de738b9fb1b0dcec53cb790b98df9787b3f9b32
rekire <git@reki.re>
2016-10-23 18:51:07 +0200

    Optimized run script

--- run.bat
+++ run.bat
@@ -1,10 +1,10 @@
 @echo off
+cls
 echo Publishing...
-call gradlew -Dfrom=scratch cle publishToMavenLocal -q
+call gradlew -Pfrom=scratch clean publishToMavenLocal -q
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo [Done]
 echo.
 echo Testing...
-REM call gradlew :examplelibrary:generateDebugPojoBoosterStubs -q
-call gradlew clean :ex:app:aDeb :ex:lib:aDeb :ex:java:jar -s
-
+REM call gradlew clean :ex:app:aDeb :ex:lib:aDeb :ex:java:jar -s
+call gradlew clean check
